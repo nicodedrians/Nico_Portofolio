a0dcde1ed6d7366c8c85bc0b20aba8e6109f84e5
Nico Dedrian <nicodedrians@gmail.com>
2023-09-27 11:16:54 +0700

    add java file

---
+++ test/hello.java
@@ -0,0 +1,8 @@
+public class hello {
+    public static void main(String[] args) {
+        helloWorld();
+    }
+    static void helloWorld(){
+        System.out.println("Hi I am Quality Assurance Enthusiast");
+    }
+}
